dee6ecb1f32436e16241bda5706553e1c537188b
hendiarji <hendiarg@gmail.com>
2022-03-10 12:27:07 +0700

    2

---
+++ aplikasi/src/materi/Komponen/pembahasan/ClassComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+  rende() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>Hello dunia</h2>
+      </div>
+    );
+  }
+}
+
+export default ClassComponent;
